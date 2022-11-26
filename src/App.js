16fe761c96559ca290981f93bb79712d8fe239e5
toto <riedotoine@gmail.com>
2022-11-26 18:09:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import data from "./data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export default function App() {
+  const date = new Date();
+  const [day, month] = [date.getDate(), date.getMonth() + 1];
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  }
+
+  const dataset = data.map((items) => {
+    let arrDate = items.date;
+    function getColor() {
+      let blue = "#3D49E8";
+      let green = "#28AB67";
+      let pink = "#EA5D8D";
+      if (Math.random() <= 0.33) {
+        return blue;
+      } else if (Math.random() >= 0.33 && Math.random() <= 0.66) {
+        return green;
+      } else return pink;
+    }
+
+    let newArrDate = arrDate.split("-");
+    if (newArrDate[2] <= day && newArrDate[1] == month) {
+      console.error("DATES SONT LES MEMES");
+      return (
+        <div
+          className="box"
+          style={{
+            height: getRandomInt(100, 200) + "px",
+            width: getRandomInt(100, 150) + "px",
+            backgroundColor: getColor(),
+            color: "black",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {items.id}
+        </div>
+      );
+    } else {
+      return (
+        <div
+          className="box"
+          style={{
+            height: getRandomInt(100, 200) + "px",
+            width: getRandomInt(100, 150) + "px",
+            backgroundColor: "#000000",
+            borderStyle: "solid",
+            borderColor: "white",
+            borderWidth: "thin",
+          }}
+        >
+          {items.id}
+        </div>
+      );
+
+      console.log(
+        "la mois n'est pas comparé:",
+        newArrDate[1],
+        "le mois juste: " + month
+      );
+    }
+  });
+  return (
+    <div className="app">
+      <div className="logo">
+        <h4 className="logotext">DONUTS</h4>
+      </div>
+      <div className="title">
+        <h1>Le calendrier de l’avent de Donuts</h1>
+        <p className="text">
+          Découvrez chaque jour une nouvelle surprise proposée par l’un de nos
+          collaborateurs
+        </p>
+      </div>
+      <div className="dataset">{dataset}</div>
     </div>
   );
 }
-
-export default App;
---
+++ src/data.js
@@ -0,0 +1,148 @@
+const data =[
+    {
+        id: 1,
+        title: "1",
+        date: "2022-11-26",
+        active: true,
+    },
+    {
+        id: 2,
+        title: "2",
+        date: "2022-11-27",
+        active: true,
+    },
+    {
+        id: 3,
+        title: "3",
+        date: "2022-11-28",
+        active: true,
+    },
+    {
+        id: 4,
+        title: "4",
+        date: "2022-11-25",
+        active: true,
+    },
+    {
+        id: 5,
+        title: "5",
+        date: "2022-11-27",
+        active: true,
+    },
+    {
+        id: 6,
+        title: "6",
+        date: "2022-11-30",
+        active: true,
+    },
+    {
+        id: 7,
+        title: "7",
+        date: "2022-12-01",
+        active: true,
+    },
+    {
+        id: 8,
+        title: "8",
+        date: "2022-11-24",
+        active: true,
+    },
+    {
+        id: 9,
+        title: "9",
+        date: "2022-11-26",
+        active: true,
+    },
+    {
+        id: 10,
+        title: "10",
+        date: "2022-11-26",
+        active: true,
+    },
+    {
+        id: 11,
+        title: "11",
+        date: "2022-11-26",
+        active: true,
+    },
+    {
+        id: 12,
+        title: "12",
+        date: "2022-11-26",
+        active: true,
+    },
+    {
+        id: 13,
+        title: "13",
+        date: "2022-11-26",
+        active: true,
+    },
+    {
+        id: 14,
+        title: "14",
+        date: "2022-11-26",
+        active: true,
+    },
+    {
+        id: 15,
+        title: "15",
+        date: "2022-11-26",
+        active: true,
+    },
+    {
+        id: 16,
+        title: "16",
+        date: "2022-11-26",
+        active: true,
+    },
+    {
+        id: 17,
+        title: "17",
+        date: "2022-11-26",
+        active: true,
+    },
+    {
+        id: 18,
+        title: "18",
+        date: "2022-11-26",
+        active: true,
+    },
+    {
+        id: 19,
+        title: "19",
+        date: "2022-11-26",
+        active: true,
+    },
+    {
+        id: 20,
+        title: "20",
+        date: "2022-11-26",
+        active: true,
+    },
+    {
+        id: 21,
+        title: "21",
+        date: "2022-11-26",
+        active: true,
+    },
+    {
+        id: 22,
+        title: "22",
+        date: "2022-11-26",
+        active: true,
+    },
+    {
+        id: 23,
+        title: "23",
+        date: "2022-11-26",
+        active: true,
+    },
+    {
+        id: 24,
+        title: "24",
+        date: "2022-11-26",
+        active: true,
+    }
+]
+
+export default data;
