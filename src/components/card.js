fb2729b6abf9524c12d94d98337a598d9e7cad0e
toto <riedotoine@gmail.com>
2022-11-30 14:05:49 +0100

    update the readme and comment the code

--- src/components/card.js
+++ src/components/card.js
@@ -4,24 +4,31 @@ import { useState } from "react";
 import {CgClose} from 'react-icons/cg'
 
 export default function Card(idCase) {
-  console.log(idCase, "il vient de card lui");
+  /**
+   * déclaration de mon useState 
+   * utilisé dans le bouton pour afficher la card ou non
+   */
   const [toggleView, setToggleView] = useState(true);
+  //récupération de l'id de la case cliquer transformer en numéro car on reçoit un objet
   const caseShow = Object.values(idCase) - 1;
+  //récupération de l'id/author/image/media dans le tableau par rapport idCase
   const showId = data[caseShow].id;
   const showAuthor = data[caseShow].author;
   const showImage = data[caseShow].image;
   const showMedia = data[caseShow].media;
 
+  //fonction qui a pour but de désactiver l'affichage de la card
   function handleClick() {
     setToggleView(!toggleView);
-    console.log(toggleView);
   }
+  //réutilisation du tableau/ function afin de pouvoir avoir les mêmes couleur que les cases
   const colorArray = ["#3D49E8", "#28AB67", "#EA5D8D"];
   function getColor() {
     var color = showId % colorArray.length;
     return color;
   }
 
+  //function pour afficher la card
   function cardView() {
     return (
       <div className="card-view">
@@ -57,8 +64,7 @@ export default function Card(idCase) {
       </div>
     );
   }
-  console.log("couleur est  ", showId);
-
+//if qui désactive l'affichage de la card lorsque toggleView == false 
   if (toggleView === true) {
     return cardView();
   } else return null;
